a00a770049654c68a939560a797fdcf66bdfa801
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-15 20:27:45 -0800

    updated source

--- game_branch.py
+++ game_branch.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
 
 import new_module
+import sys
 
 print("Welcome to my Classes Python program! Do you want", 
     "to go through Door #1 or Door #2?")
 
 answer = input("> ")
 
-if answer == "1":
-    print("What do you want to do?")
-    print("1. Press (1) to Win.")
-    print("2. Press (2) to Die.")
+class Intro():
 
-    answer_2 = input("> ")
+    def __init__ (self, beginning, end):
+        self.String1 = "Start_1"
+        self.String2 = "End_2"
 
-    if answer_2 == "1":
-        print("You pressed (1)")
-    elif answer_2 == "2":
-        print("You pressed (2)")
-    else:
-        print(f"Well, doing {answer_2}")
-    
-    the_count = [5, 4, 3, 2, 1]
-    
-    for i in the_count:
-        print(f"And you die in...: {i}")
+    def start_one (self):
 
-elif answer == "2":
-    print("you pressed 2")
+        if answer == "1":
+            print("What do you want to do?")
+            print("1. Press (1) to Win.")
+            print("2. Press (2) to Die.")
+
+            answer_2 = input("> ")
+
+        if answer_2 == "1":
+            print("You pressed (1)") # You Win
+
+        elif answer == "2":
+            print("you pressed 2") # You Die
+            sys.exit() 
+
+        else:
+            print(f"Well, doing {answer_2}")
+            print("Error!")
+            sys.exit()
+
+        the_count = [5, 4, 3, 2, 1]
+
+        for i in the_count:
+            print(f"And you die in...: {i}")
 
 class Finished ():
 
@@ -44,3 +55,13 @@ class Finished ():
 class Redundent ():
     def print_function(self, output):
         self.output = output
+
+class Person:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
