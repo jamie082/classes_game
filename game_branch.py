e323e576ab88bf7e3b3b88784bd78c4da22f0bb9
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-16 14:18:06 -0800

    updated working class source

--- game_branch.py
+++ game_branch.py
@@ -3,50 +3,57 @@
 import new_module
 import sys
 
-print("Welcome to my Classes Python program! Do you want",
-            "to go through Door #1 or Door #2?")
+print("""Welcome to my Classes Python program! Do you want.
+      to go through Door #1 or Door #2?""")
 
 answer = input("> ")
 
 class MainProgram ():
 
-    def identify (name):
-        print(f"You won,  {name}")
+        if answer == "1":
+            print("What do you want to do?")
+            print("1. To you want to Won Game press (1)")
+            print("2. Do you want to Lost Game, press (2)")
 
-    if answer == "1":
-        print("What do you want to do?")
-        print("1. Press (1) to Win.")
-        print("2. Press (2) to Die.")
+            answer_2 = input("> ")
 
-        answer_2 = input("> ")
+            if answer_2 == "1":
+                print("You pressed (1)") # You Lost, go to function
 
-        if answer_2 == "1":
-            print("You pressed (1)") # You Win, go to function
+            elif answer_2 == "2":
+              print("you pressed 2") # Won Game
 
-        elif answer_2 == "2":
-           print("you pressed 2") # You Die
+            else:
+                 print(f"Wrong selection, {answer_2}")
+                 print("Error!")
 
-        else:  
-            print(f"Wrong selection, {answer_2}")
+            the_count = [5, 4, 3, 2, 1]
+
+            for i in the_count:
+                print(f"And you lose in...: {i}")
+
+        elif answer == "2":
+            print ("you pressed (2)")
+            print ("You lose")
+            print ("Enter function")
+
+        else:
+            print(f"Wrong selection, {answer}")
             print("Error!")
 
-        the_count = [5, 4, 3, 2, 1]
+class Lost_Game(): # Lost class
 
-        for i in the_count:
-            print(f"And you die in...: {i}")
+    def print_function (name):
+        print(f"You {name}")
 
-class End_Game():
+        sys.exit()
 
-    def print_function(self):
+class You_Won(): # Win class
 
-        i = 0
-        while count < 5:  # output bzzzzd'd 5 times
-            print ("BZZZZZZD! EOF!!")
-            i += count
+    def identify (name):
+        print(f"You {name}")
 
-class You_Won():
+        sys.exit()
 
-    def win (self):
-        var.identify()
-
-var = MainProgram.identify('jamie')
+var_2 = Lost_Game.print_function('lost')
+var = You_Won.identify('won')
