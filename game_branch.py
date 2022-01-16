4518bd321c1168b589584ef8e3b474828c1f6205
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-16 00:18:31 -0800

    updated source, two classes

--- game_branch.py
+++ game_branch.py
@@ -3,65 +3,47 @@
 import new_module
 import sys
 
-print("Welcome to my Classes Python program! Do you want", 
-    "to go through Door #1 or Door #2?")
+print("Welcome to my Classes Python program! Do you want",
+            "to go through Door #1 or Door #2?")
 
 answer = input("> ")
 
-class Intro():
+class MainProgram ():
+    def identify (name):
+        print(f"You won,  {name}")
 
-    def __init__ (self, beginning, end):
-        self.String1 = "Start_1"
-        self.String2 = "End_2"
+    if answer == "1":
+        print("What do you want to do?")
+        print("1. Press (1) to Win.")
+        print("2. Press (2) to Die.")
 
-    def start_one (self):
-
-        if answer == "1":
-            print("What do you want to do?")
-            print("1. Press (1) to Win.")
-            print("2. Press (2) to Die.")
-
-            answer_2 = input("> ")
+        answer_2 = input("> ")
 
         if answer_2 == "1":
             print("You pressed (1)") # You Win
 
         elif answer == "2":
-            print("you pressed 2") # You Die
-            sys.exit() 
+           print("you pressed 2") # You Die
 
         else:
             print(f"Well, doing {answer_2}")
             print("Error!")
-            sys.exit()
 
         the_count = [5, 4, 3, 2, 1]
 
         for i in the_count:
             print(f"And you die in...: {i}")
 
-class Finished ():
+class End_Game():
 
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+    def print_function(self):
 
-    def swim(self):
-        print("The fish is swimming.")
+        i = 0
+        while count < 5:  # output bzzzzd'd 5 times
+            print ("BZZZZZZD!")
+            i += count
 
-    def swim_backwards (self):
-        print("You won! Good job.")
+class You_Won():
 
-class Redundent ():
-    def print_function(self, output):
-        self.output = output
-
-class Person:
-    def __init__ (self, name, age):
-        self.name = name
-        self.age = age
-
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
+    def win (self):
+        MainProgram.identify('jamie')
