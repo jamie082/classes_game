36e592dc0885a14f0fc9f747e558634d30ea44ec
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-01-16 17:28:31 -0800

    updated source

--- game_branch.py
+++ game_branch.py
@@ -3,8 +3,8 @@
 import new_module
 import sys
 
-print("""Welcome to my Classes Python program! Do you want.
-      to go through Door #1 or Door #2?""")
+print("""Welcome to my Classes Python program! Do you want,
+        to (C)ontinue game or (Q)uit game? Press "1" or "2""")
 
 answer = input("> ")
 
@@ -12,48 +12,57 @@ class MainProgram ():
 
         if answer == "1":
             print("What do you want to do?")
-            print("1. To you want to Won Game press (1)")
-            print("2. Do you want to Lost Game, press (2)")
+            print("1. Do you want to Win the game, press (1)")
+            print("2. Do you want to Lose the game, press (2)")
 
             answer_2 = input("> ")
 
             if answer_2 == "1":
                 print("You pressed (1)") # You Lost, go to function
+                print("You win")
 
             elif answer_2 == "2":
               print("you pressed 2") # Won Game
+              print("You lost")
 
             else:
                  print(f"Wrong selection, {answer_2}")
                  print("Error!")
 
-            the_count = [5, 4, 3, 2, 1]
-
-            for i in the_count:
-                print(f"And you lose in...: {i}")
-
         elif answer == "2":
             print ("you pressed (2)")
             print ("You lose")
-            print ("Enter function")
 
         else:
             print(f"Wrong selection, {answer}")
             print("Error!")
 
-class Lost_Game(): # Lost class
-
+class Lost_Game():
     def print_function (name):
         print(f"You {name}")
 
         sys.exit()
 
-class You_Won(): # Win class
+class Win_Game():
 
-    def identify (name):
+    def win_function (name):
         print(f"You {name}")
 
         sys.exit()
 
+class End_Function:
+    
+    def __init__ (self, first, last):
+        self.first = first
+        self.last = last
+
+    def details (self):
+        print("First :", self.first)
+        print("Last  :", self.last)
+
+# create object
+
+struct = End_Function('Jamie', 'Morrissey')
+struct.details()
 var_2 = Lost_Game.print_function('lost')
 var = You_Won.identify('won')
